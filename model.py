6c9c4f53d1124bed00199a66645d3573daf7adb2
aleksey.shimko <aleksey.shimko@X5.ru>
2022-01-04 12:17:52 +0300

    LinSVC; add feature selection by chi2; cv-0.75806

--- model.py
+++ model.py
@@ -1,19 +1,25 @@
 from typing import Tuple
-from numpy import int32
+from numpy import int32, vectorize
 from sklearn.compose import ColumnTransformer
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 from sklearn.multioutput import ClassifierChain, MultiOutputClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.svm import LinearSVC
+from sklearn.feature_selection import chi2, SelectPercentile
 
 from config import NEGATIVE, ORDERED_CATEGORIES, POSITIVE, UNORDERED_CATEGORIES
 
 
 def make_model(n_splits: int = 5, random_state: int = 42) -> Tuple[Pipeline, bool]:
+    text_processing = Pipeline(memory='.cache', verbose=True, steps=[
+        ('vectorize', CountVectorizer(dtype=int32)),
+        ('select_features', SelectPercentile(chi2, percentile=25)),
+        ('tfidf', TfidfTransformer())
+    ])
     features_generation = ColumnTransformer(n_jobs=-1, verbose=True, transformers=[
-        ('positive_col', CountVectorizer(dtype=int32), POSITIVE),
-        ('negative_col', CountVectorizer(dtype=int32), NEGATIVE),
+        ('positive_col', text_processing, POSITIVE),
+        ('negative_col', text_processing, NEGATIVE),
         ('ordered_categories', 'passthrough', ORDERED_CATEGORIES),
         ('unordered_categories', OneHotEncoder(dtype=int32, handle_unknown='ignore'), UNORDERED_CATEGORIES)
     ])
