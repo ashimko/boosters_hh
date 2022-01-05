bc8f4cb85df1bd0bc07ec90f057fa95f2d4f067f
aleksey.shimko <aleksey.shimko@X5.ru>
2022-01-05 12:52:58 +0300

    revert 922245c2b model.py

--- model.py
+++ model.py
@@ -8,26 +8,32 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.svm import LinearSVC
 from sklearn.feature_selection import chi2, SelectPercentile
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.linear_model import RidgeClassifier
+from sklearn.linear_model import LogisticRegressionCV
 
 from config import NEGATIVE, ORDERED_CATEGORIES, POSITIVE, UNORDERED_CATEGORIES
 
 
 def make_model(n_splits: int = 5, random_state: int = 42) -> Tuple[Pipeline, bool]:
-    text_processing = Pipeline(memory='.cache', verbose=True, steps=[
-        ('vectorize', CountVectorizer(dtype=int32)),
-        ('select_features', SelectPercentile(chi2, percentile=25)),
-        ('tfidf', TfidfTransformer())
-    ])
+    # text_processing = Pipeline(memory='.cache', verbose=True, steps=[
+    #     ('vectorize', CountVectorizer(dtype=int32)),
+    #     ('select_features', SelectPercentile(chi2, percentile=25)),
+    #     ('tfidf', TfidfTransformer())
+    # ])
+
     features_generation = ColumnTransformer(n_jobs=-1, verbose=True, transformers=[
-        ('positive_col', text_processing, POSITIVE),
-        ('negative_col', text_processing, NEGATIVE),
+        ('positive_col', CountVectorizer(dtype=int32), POSITIVE),
+        ('negative_col', CountVectorizer(dtype=int32), NEGATIVE),
         ('ordered_categories', 'passthrough', ORDERED_CATEGORIES),
         ('unordered_categories', OneHotEncoder(dtype=int32, handle_unknown='ignore'), UNORDERED_CATEGORIES)
     ])
-    base_estimator = AdaBoostClassifier(base_estimator=RidgeClassifier(), algorithm='SAMME', random_state=random_state)
+
+    base_estimator = LogisticRegressionCV(n_jobs=-1)
     model = Pipeline(memory='.cache', verbose=True, steps=[
         ('get_features', features_generation),
-        ('model', MultiOutputClassifier(estimator=base_estimator, n_jobs=-1))
+        ('model', ClassifierChain(
+            base_estimator=base_estimator,
+            order='random', 
+            cv=n_splits, 
+            random_state=random_state))
     ])
     return model, hasattr(base_estimator, 'predict_proba')
