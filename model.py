025012465e1fd4b8b90d07127b3e8cdea4045f69
aleksey.shimko <aleksey.shimko@X5.ru>
2022-01-05 10:06:01 +0300

    LGB; cv-0.78995

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.svm import LinearSVC
 from sklearn.feature_selection import chi2, SelectPercentile
-from catboost import CatBoostClassifier
+from lightgbm import LGBMClassifier
 
 from config import NEGATIVE, ORDERED_CATEGORIES, POSITIVE, UNORDERED_CATEGORIES
 
@@ -24,7 +24,7 @@ def make_model(n_splits: int = 5, random_state: int = 42) -> Tuple[Pipeline, boo
         ('ordered_categories', 'passthrough', ORDERED_CATEGORIES),
         ('unordered_categories', OneHotEncoder(dtype=int32, handle_unknown='ignore'), UNORDERED_CATEGORIES)
     ])
-    base_estimator = CatBoostClassifier(silent=True)
+    base_estimator = LGBMClassifier()
     model = Pipeline(memory='.cache', verbose=True, steps=[
         ('get_features', features_generation),
         ('model', MultiOutputClassifier(estimator=base_estimator, n_jobs=1))
