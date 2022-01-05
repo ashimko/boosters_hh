74c9c136f831b5ec0931c29e847220cf5f7d40be
aleksey.shimko <aleksey.shimko@X5.ru>
2022-01-05 23:45:33 +0300

    LinSVC; cv-0.78121

--- model.py
+++ model.py
@@ -5,13 +5,15 @@ from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 from sklearn.multioutput import ClassifierChain, MultiOutputClassifier
 from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.svm import LinearSVC
 from sklearn.feature_selection import chi2, SelectPercentile
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.ensemble import RandomForestClassifier
 from lightgbm import LGBMClassifier
+from sklearn.decomposition import TruncatedSVD
+import category_encoders as ce
 
 from config import NEGATIVE, ORDERED_CATEGORIES, POSITIVE, UNORDERED_CATEGORIES
 
@@ -19,11 +21,11 @@ from config import NEGATIVE, ORDERED_CATEGORIES, POSITIVE, UNORDERED_CATEGORIES
 def make_model(n_splits: int = 5, random_state: int = 42) -> Tuple[Pipeline, bool]:
     text_processing = Pipeline(memory='.cache', verbose=True, steps=[
         ('vectorize', FeatureUnion(n_jobs=-1, transformer_list=[
-            ('count_vec_char_wb', CountVectorizer(analyzer='char_wb', dtype=int32)),
-            ('count_vec_word', CountVectorizer(analyzer='word', dtype=int32))])
+            ('count_vec_char_wb', CountVectorizer(analyzer='char_wb', ngram_range=(1,4), dtype=int32)),
+            ('count_vec_word', CountVectorizer(analyzer='word', ngram_range=(1,3), dtype=int32))])
             ),
         ('select_features', SelectPercentile(chi2, percentile=35)),
-        ('tfidf', TfidfTransformer())
+        ('tfidf', TfidfTransformer()),
     ])
 
     features_generation = ColumnTransformer(n_jobs=-1, verbose=True, transformers=[
@@ -31,7 +33,7 @@ def make_model(n_splits: int = 5, random_state: int = 42) -> Tuple[Pipeline, boo
         ('negative_col', text_processing, NEGATIVE),
         ('ordered_categories_as_is', 'passthrough', ORDERED_CATEGORIES),
         ('ordered_categories_ohe', OneHotEncoder(dtype=bool_, handle_unknown='ignore'), ORDERED_CATEGORIES),
-        ('unordered_categories', OneHotEncoder(dtype=bool_, handle_unknown='ignore'), UNORDERED_CATEGORIES)
+        ('unordered_categories', OneHotEncoder(dtype=bool_, handle_unknown='ignore'), UNORDERED_CATEGORIES),
     ])
 
     base_estimator = LinearSVC()
