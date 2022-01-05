0522cfe36f6d328a2cd310caeadc2fba9a9189d1
aleksey.shimko <aleksey.shimko@X5.ru>
2022-01-05 18:32:35 +0300

    Add char_wb; cv-0.7725

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from numpy import int32, vectorize
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 from sklearn.multioutput import ClassifierChain, MultiOutputClassifier
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.svm import LinearSVC
 from sklearn.feature_selection import chi2, SelectPercentile
@@ -17,20 +17,23 @@ from config import NEGATIVE, ORDERED_CATEGORIES, POSITIVE, UNORDERED_CATEGORIES
 
 
 def make_model(n_splits: int = 5, random_state: int = 42) -> Tuple[Pipeline, bool]:
-    # text_processing = Pipeline(memory='.cache', verbose=True, steps=[
-    #     ('vectorize', CountVectorizer(dtype=int32)),
-    #     ('select_features', SelectPercentile(chi2, percentile=25)),
-    #     ('tfidf', TfidfTransformer())
-    # ])
+    text_processing = Pipeline(memory='.cache', verbose=True, steps=[
+        ('vectorize', FeatureUnion(n_jobs=-1, transformer_list=[
+            ('count_vec_word', CountVectorizer(analyzer='word', dtype=int32)),
+            ('count_vec_char_wb', CountVectorizer(analyzer='char_wb', dtype=int32))
+            ])),
+        ('select_features', SelectPercentile(chi2, percentile=35)),
+        ('tfidf', TfidfTransformer())
+    ])
 
     features_generation = ColumnTransformer(n_jobs=-1, verbose=True, transformers=[
-        ('positive_col', CountVectorizer(dtype=int32), POSITIVE),
-        ('negative_col', CountVectorizer(dtype=int32), NEGATIVE),
+        ('positive_col', text_processing, POSITIVE),
+        ('negative_col', text_processing, NEGATIVE),
         ('ordered_categories', 'passthrough', ORDERED_CATEGORIES),
         ('unordered_categories', OneHotEncoder(dtype=int32, handle_unknown='ignore'), UNORDERED_CATEGORIES)
     ])
 
-    base_estimator = LGBMClassifier(random_state=random_state)
+    base_estimator = LinearSVC()
     model = Pipeline(memory='.cache', verbose=True, steps=[
         ('get_features', features_generation),
         ('model', MultiOutputClassifier(estimator=base_estimator, n_jobs=-1))
