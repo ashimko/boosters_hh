feadd01bcb8f60852f1627386029db79cfd177c0
Aleksei Shimko <shimko.a.a@gmail.com>
2022-01-06 09:09:07 +0300

    XGB; cv-0.8

--- model.py
+++ model.py
@@ -13,8 +13,8 @@ from sklearn.linear_model import LogisticRegressionCV
 from sklearn.ensemble import RandomForestClassifier
 from lightgbm import LGBMClassifier
 from sklearn.decomposition import TruncatedSVD
-import category_encoders as ce
 from catboost import CatBoostClassifier
+from xgboost import XGBClassifier
 
 from config import NEGATIVE, ORDERED_CATEGORIES, POSITIVE, UNORDERED_CATEGORIES
 
@@ -37,9 +37,9 @@ def make_model(n_splits: int = 5, random_state: int = 42) -> Tuple[Pipeline, boo
         ('unordered_categories', OneHotEncoder(dtype=int8, handle_unknown='ignore'), UNORDERED_CATEGORIES),
     ])
     
-    base_estimator = LogisticRegressionCV(Cs=20, n_jobs=-1, cv=n_splits, scoring='f1_samples', random_state=random_state)
+    base_estimator = XGBClassifier(random_state=random_state)
     model = Pipeline(memory='.cache', verbose=True, steps=[
         ('get_features', features_generation),
-        ('model', MultiOutputClassifier(estimator=base_estimator, n_jobs=-1))
+        ('model', MultiOutputClassifier(estimator=base_estimator, n_jobs=1))
     ])
     return model, hasattr(base_estimator, 'predict_proba')
