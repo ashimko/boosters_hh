b9f2a269747ae7df3c181c022351933af4039fe5
Aleksei Shimko <shimko.a.a@gmail.com>
2022-01-18 20:55:53 +0300

    bert embedding

--- model.py
+++ model.py
@@ -4,12 +4,14 @@ import tensorflow as tf
 import tensorflow_addons as tfa
 from tensorflow import keras
 from tensorflow.keras import layers
+import tensorflow_text as text
 from typing import Dict
+import tensorflow_hub as hub
 from config import TEXT_COLS, ORDERED_CATEGORIES, UNORDERED_CATEGORIES
 
 
 VOCAB_SIZE = 15000
-BATCH_SIZE = 128
+BATCH_SIZE = 32
 N_TARGETS = 9
 N_EPOCHS = 20
 
@@ -23,13 +25,16 @@ def get_model_input(data: pd.DataFrame) -> Dict:
 
 def make_model(encoders: Dict) -> keras.Model:
 
-    def _get_text_model(input, encoder):
-        x = encoder(input)
-        x = layers.Embedding(
-                input_dim=len(encoder.get_vocabulary()),
-                output_dim=64,
-                mask_zero=True)(x)
-        x = layers.Bidirectional(tf.keras.layers.GRU(64, return_sequences=True))(x)
+    def _get_text_model(text_input):
+        preprocessor = hub.KerasLayer(
+            "https://tfhub.dev/tensorflow/bert_multi_cased_preprocess/3")
+        encoder_inputs = preprocessor(text_input)
+        encoder = hub.KerasLayer(
+            "https://tfhub.dev/tensorflow/bert_multi_cased_L-12_H-768_A-12/4",
+            trainable=True)
+        outputs = encoder(encoder_inputs)
+        sequence_output = outputs["sequence_output"]  # [batch_size, seq_length, 768].
+        x = layers.Bidirectional(tf.keras.layers.GRU(64, return_sequences=True))(sequence_output)
         x = layers.Dropout(0.7)(x)
         x = layers.Bidirectional(tf.keras.layers.GRU(64))(x)
         x = layers.Dropout(0.7)(x)
@@ -62,12 +67,12 @@ def make_model(encoders: Dict) -> keras.Model:
         x = layers.Dense(N_TARGETS, activation='sigmoid')(x)
         return x
 
-    text_inputs = {col: keras.Input(shape=(None,), dtype='string', name=f'{col}_text') for col in TEXT_COLS + UNORDERED_CATEGORIES}
-    unordered_cat_inputs = {col: keras.Input(shape=(None,), dtype='string', name=f'{col}_unordered_cat') for col in UNORDERED_CATEGORIES}
+    text_inputs = {col: keras.Input(shape=(), dtype=tf.string, name=f'{col}_text') for col in TEXT_COLS + UNORDERED_CATEGORIES}
+    unordered_cat_inputs = {col: keras.Input(shape=(None,), dtype=tf.string, name=f'{col}_unordered_cat') for col in UNORDERED_CATEGORIES}
     ordered_cat_input = keras.Input(shape=(len(ORDERED_CATEGORIES)), name='ordered_cat_input')
 
     ordered_cat_features = [_get_ordered_category_model(ordered_cat_input)]
-    text_features = [_get_text_model(text_inputs[col], encoders[col]) for col in TEXT_COLS]
+    text_features = [_get_text_model(text_inputs[col]) for col in TEXT_COLS]
     unordered_cat_features = [_get_unordered_category_mode(unordered_cat_inputs[col], encoders[col]) for col in UNORDERED_CATEGORIES]
 
     features = layers.Add()(ordered_cat_features + text_features + unordered_cat_features)
