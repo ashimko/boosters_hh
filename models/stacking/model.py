430d37fca1790f12c6d7d4f6b57abbde6b204519
aleksey.shimko <aleksey.shimko@X5.ru>
2022-02-18 13:48:04 +0300

    add calibrated classifier to stacking; cv-0.85038

--- models/stacking/model.py
+++ models/stacking/model.py
@@ -5,6 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.multioutput import MultiOutputClassifier
 from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
+from sklearn.calibration import CalibratedClassifierCV
 from sklearn.pipeline import Pipeline
 from model_config import N_SPLITS, RANDOM_STATE
 
@@ -12,9 +13,10 @@ from model_config import N_SPLITS, RANDOM_STATE
 def get_model(random_state: int = 42) -> Tuple[Pipeline, bool]:
 
     base_model = LogisticRegression(solver='liblinear', max_iter=1000, random_state=random_state)
-    model = MultiOutputClassifier(estimator=base_model, n_jobs=-1)
+    calibrated_model = CalibratedClassifierCV(base_model, cv=N_SPLITS, n_jobs=-1, ensemble=True)
+    model = MultiOutputClassifier(estimator=calibrated_model, n_jobs=-1)
     
-    params = {'estimator__C': np.random.uniform(0.1, 25, 10)}
+    params = {'estimator__base_estimator__C': np.random.uniform(0.1, 25, 10)}
     cv = MultilabelStratifiedKFold(n_splits=N_SPLITS, shuffle=True, random_state=RANDOM_STATE)
     gscv = GridSearchCV(
         estimator=model, 
