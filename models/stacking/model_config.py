3df39b9e95d7e14e300a66e370d307c565852f97
aleksey.shimko <aleksey.shimko@X5.ru>
2022-02-18 13:29:36 +0300

    change 5->3 splits in cv stacking; cv-0.85037

--- models/stacking/model_config.py
+++ models/stacking/model_config.py
@@ -1,5 +1,7 @@
 import os
 MODEL_NAME = os.path.basename(os.path.dirname(__file__))
-N_SPLITS = 5
+N_SPLITS = 3
 RANDOM_STATE = 79
-MODEL_NAMES = ['keras', 'catboost_labse_en_ru', 'sklearn', 'catboost', 'catboost_rubert_tiny', 'catboost_sbert_large_nlu_ru', 'catboost_handcrafted']
+MODEL_NAMES = [
+    'keras', 'catboost_labse_en_ru', 'sklearn', 'catboost',
+    'catboost_rubert_tiny', 'catboost_sbert_large_nlu_ru', 'catboost_handcrafted']
